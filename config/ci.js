929fc9cf0e01e0cd85a6453b7df4b426025fff13
Philippe Doreau <Philippe@MacBook-Pro-de-Philippe.local>
2021-04-10 21:58:18 +0200

    first commit

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '63229941982-t3qoqimlauigo9aekkoi6821snhfobjp.apps.googleusercontent.com',
+  googleClientSecret: 'CN05jUD-UTU4sCmczIDnJYuJ',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis:127.0.0.1:6379',
+};
---
+++ config/dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID:
+    '63229941982-t3qoqimlauigo9aekkoi6821snhfobjp.apps.googleusercontent.com',
+  googleClientSecret: 'CN05jUD-UTU4sCmczIDnJYuJ',
+  mongoURI:
+    'mongodb+srv://sick:dadfba16@sickcluster.iiy0s.mongodb.net/blog_dev?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis:127.0.0.1:6379',
+};
